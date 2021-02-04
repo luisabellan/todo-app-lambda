bd390ee9e74b51a58e7891556b0c79218dbf393a
Luis Abellan <luis.abellan.dev@gmail.com>
2021-02-04 13:58:56 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import React from "react";
-import TodoList from "./components/TodoComponents/TodoList"
-import TodoForm from "./components/TodoComponents/TodoForm"
-import "./App.css"
+import React from 'react';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+import './App.css';
 
-const todoItems = [
-  
+const todoItems = [];
 
-]
-
-let todoItem = "";
-
-
-
+let todoItem = '';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
 
-
   constructor() {
-
-    super()
+    super();
     this.state = {
       todoItems,
       todoItem,
@@ -37,81 +29,63 @@ class App extends React.Component {
     const newItem = {
       name: item,
       id: Date.now(),
-      purchased: false
+      purchased: false,
     };
 
-    
     this.setState({
       todoItems: [...this.state.todoItems, newItem],
-     
     });
-
-   
-  }
-
-   
-    
-  
+  };
 
   // this is a method of App
-  toggleItem = itemId => {
+  toggleItem = (itemId) => {
     console.log(itemId);
 
     this.setState({
-      todoItems: this.state.todoItems.map(item => {
+      todoItems: this.state.todoItems.map((item) => {
         console.log(item);
         if (itemId === item.id) {
           return {
             ...item,
-            purchased: !item.purchased
+            purchased: !item.purchased,
           };
         }
 
         return item;
-      })
+      }),
     });
   };
 
-  clearPurchased = e => {
+  clearPurchased = (e) => {
     e.preventDefault();
     console.log(this.state.todoItems);
     this.setState({
       // returns the items that haven't been purchased and purges
       // the ones that have been purchased
-      todoItems: this.state.todoItems.filter(item => item.purchased === false)
+      todoItems: this.state.todoItems.filter(
+        (item) => item.purchased === false
+      ),
     });
-    console.log(this.state.todoItems);
+
   };
 
-  
-
   render() {
-
     return (
-      <div className="app-wrapper">
-        <h2>Welcome to My Todo App!</h2>
-        <TodoForm
-          addItem={this.addItem}
-          clearPurchased={this.clearPurchased}
-        />
+      <div className='app-wrapper'>
+        <h2>Todo App</h2>
+        <TodoForm addItem={this.addItem} clearPurchased={this.clearPurchased} />
         <TodoList
           todoItems={this.state.todoItems}
           toggleItem={this.toggleItem}
-
-
         />
-
-        <p className="copyright">Copyright {new Date().getFullYear()} - Luis Abellan</p>
-
-
-
-
+        <footer>
+          <p className='copyright'>
+            Copyright {new Date().getFullYear()} - Luis Abellan
+          </p>
+        </footer>
       </div>
     );
   }
 }
-  
 
-
-
-export default App
+export default App;
