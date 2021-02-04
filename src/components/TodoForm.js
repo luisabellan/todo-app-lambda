b5fd1e6317589cd5056542d6088656e23223baf4
Luis Abellan <luis.abellan.dev@gmail.com>
2021-02-04 13:17:43 +0100

    save

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
 class TodoForm extends React.Component {
   constructor() {
     super();
     this.state = {
-      todoItem1: ""
+      todoItem1: '',
     };
   }
 
-  handleChanges = e => {
+  handleChanges = (e) => {
     // update state with each keystroke
     // console.log(e)
     this.setState({ [e.target.name]: e.target.value });
@@ -16,29 +16,29 @@ class TodoForm extends React.Component {
   };
 
   // class property to submit form
-  submitItem = e => {
-    e.preventDefault()
-    console.log(this.props)
-    this.props.addItem(e, this.state.todoItem1)
-    this.setState({ todoItem1: "" })
+  submitItem = (e) => {
+    e.preventDefault();
+    console.log(this.props);
+    this.props.addItem(e, this.state.todoItem1);
+    this.setState({ todoItem1: '' });
   };
 
   render() {
-    console.log("rendering form", this.state.todoItem);
-    
+    console.log('rendering form', this.state.todoItem);
+
     return (
       <form onSubmit={this.submitItem}>
         <input
-          type="text"
-          value= {this.state.todoItem1}
-          placeholder="Add new task here"
-          name="todoItem1"
+          type='text'
+          value={this.state.todoItem1}
+          placeholder='Add new task here'
+          name='todoItem1'
           onChange={this.handleChanges}
         ></input>
-        <button className="add-to-do-btn">
+        <button className='add-to-do-btn'>
           <span>Add Todo</span>
         </button>
-        <button className="clear-btn" onClick={this.props.clearPurchased}>
+        <button className='clear-btn' onClick={this.props.clearPurchased}>
           <span>Clear List</span>
         </button>
       </form>
